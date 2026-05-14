86d61bd1fa1cc6b02bb2b1198d219d72819664c8
Sami Siraj <sami.siraj.s.n2004@gmail.com>
2026-05-15 01:56:45 +0530

    feat: add bookmarking function bm

--- bookmark.sh
+++ bookmark.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
-echo $1
+
+BOOKMARK_FILE="$HOME/.bookmarks"
+
+touch $BOOKMARK_FILE
+
+bm(){
+	local name=$1
+	local path=$(pwd)
+
+	if [ -z "$name"];
+	then
+        echo "Usage: bm <name>"
+        return 1
+    fi
+    
+
+    if grep -q "^$name" $BOOKMARK_FILE ;
+    then
+        sed -i '' "s|^$name=.*|$name=$path" BOOKMARK_File
+        echo "updated bookmark '$name' -> $path"
+    else
+        echo "$name=$path" >> $BOOKMARK_FILE
+        echo "saved bookmark '$name' -> $path"
+    fi
+}
