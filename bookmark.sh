fef22f8c936556163d8101cd255cb14e78efb9c9
Sami Siraj <sami.siraj.s.n2004@gmail.com>
2026-05-16 03:07:13 +0530

    feat: add function go to jump to saved directory

--- bookmark.sh
+++ bookmark.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 
 BOOKMARK_FILE="$HOME/.bookmarks"
 
@@ -8,19 +8,38 @@ bm(){
 	local name=$1
 	local path=$(pwd)
 
-	if [ -z "$name"];
+	if [ -z "$name" ];
 	then
         echo "Usage: bm <name>"
         return 1
     fi
     
 
-    if grep -q "^$name" $BOOKMARK_FILE ;
+    if /usr/bin/grep -q "^$name=" $BOOKMARK_FILE ;
     then
-        sed -i '' "s|^$name=.*|$name=$path" BOOKMARK_File
+        /usr/bin/sed -i '' "s|^$name=.*|$name=$path|" $BOOKMARK_FILE
         echo "updated bookmark '$name' -> $path"
     else
         echo "$name=$path" >> $BOOKMARK_FILE
         echo "saved bookmark '$name' -> $path"
     fi
+}
+go(){
+    local name=$1
+    local path=$(grep "^$name" $BOOKMARK_FILE | cut -d "=" -f 2)
+
+    if [ -z "$name" ];
+    then
+        echo "Usage: go <name>"
+        return 1
+    fi
+
+    if [ -z "$path" ];
+    then
+        echo "bookmark $name not found"
+        return 1
+    fi
+
+    cd "$path"
+    echo "changed directory to '$name' -> $path"
 }
